3e8eb751c51a13f59a4b81c3410140c0c88fe621
TheIndra55 <wuppie55@gmail.com>
2017-09-24 19:18:19 +0200

    started on server

---
+++ MinecartSharp/Config.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Newtonsoft.Json;
+
+namespace MinecartSharp
+{
+    class Config
+    {
+
+        public int Port { get; set; } = 25565;
+        public string World { get; set; } = "World";
+        [JsonProperty("Max-players")]
+        public int MaxPlayer { get; set; } = 16;
+
+        public string Motd { get; set; }
+        [JsonProperty("Online-mode")]
+        public bool Whitelist { get; set; } = false;
+        public bool OnlineMode { get; set; } = true;
+
+        [JsonProperty("Resource-pack")]
+        public string ResourcePack { get; set; } = "";
+
+        public List<String> Ops { get; set; } = new List<string>()
+        {
+            "TheIndra"
+        };
+
+    }
+}
--- MinecartSharp/Program.cs
+++ MinecartSharp/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +11,27 @@ namespace MinecartSharp
 {
     class Program
     {
+
+        public static Config Configuration = new Config();
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Starting MinecartSharp v{0}", Assembly.GetExecutingAssembly().GetName().Version);
+
+            // other things?
+
+            Console.WriteLine("Loading server configuration");
+            if (!File.Exists("config.json"))
+            {
+                using (StreamWriter file = File.CreateText("config.json"))
+                {
+                    file.Write(JsonConvert.SerializeObject(new Config(), Formatting.Indented));
+                }
+            }
+
+            Configuration = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json"));
+
+
         }
     }
 }
