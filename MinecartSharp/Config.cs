527c8cdff1c3a5223bab90f74e9d7827899669c2
TheIndra55 <wuppie55@gmail.com>
2017-09-25 16:57:38 +0200

    code cleanup

--- MinecartSharp/Config.cs
+++ MinecartSharp/Config.cs
@@ -1,10 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Dynamic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Schema;
 using Newtonsoft.Json;
 
 namespace MinecartSharp
--- MinecartSharp/Networking/Listener.cs
+++ MinecartSharp/Networking/Listener.cs
@@ -1,14 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Net;
-using System.Net.Configuration;
-using System.Net.Http;
 using System.Net.Sockets;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using MinecartSharp.Utils;
 
@@ -53,7 +45,7 @@ namespace MinecartSharp.Networking
 
             while (true)
             {
-
+                //TODO: write packets system
             }
             
         }
