5ffa6d3b8410ca23ce31ec2fb00c8e735a75f783
TheIndra55 <wuppie55@gmail.com>
2017-09-29 22:37:16 +0200

    fixed project files...

---
+++ MinecartSharp/Globals.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using MinecartSharp.MinecaftSharp.Networking.Interfaces;
+using MinecartSharp.MinecartSharp.Networking.Packets;
+
+namespace MinecartSharp.MinecartSharp.Networking
+{
+    internal class Globals
+    {
+        public static string ProtocolName = "MinecartSharp 1.12";
+        public static int ProtocolVersion = 340;
+        public static int MaxPlayers = 10;
+        public static int PlayersOnline = 0;
+        public static string ServerMOTD = "Themepark lol";
+        internal static List<IPacket> Packets = new List<IPacket>();
+
+        internal static void setupPackets()
+        {
+            Packets.Add(new HandshakePacket());
+            Packets.Add(new LoginSuccess());
+        }
+    }
+}
